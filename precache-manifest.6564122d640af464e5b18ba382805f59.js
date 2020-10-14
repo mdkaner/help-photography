84a8c207a56123110175446adfe269743c41769c
mdkaner <mdkaner@users.noreply.github.com>
2020-10-14 17:22:07 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.6564122d640af464e5b18ba382805f59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9922a543ca82293adebcec167e002b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972bec008d31439591ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "556b311ba78f24353348",
+    "url": "/static/js/2.77045362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77045362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972bec008d31439591ab",
+    "url": "/static/js/main.c200d643.chunk.js"
+  },
+  {
+    "revision": "10ec119e7fafa3a51566",
+    "url": "/static/js/runtime-main.364e8317.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
